43f41bb2d6136d07b078b41f4ff656bab71960bc
bresbence <bresbence@gmail.com>
2025-04-13 20:36:30 +0200

    CodeMonkey_2_1_14

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -2,39 +2,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-  [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private float moveSpeed = 7f;
+    [SerializeField] private GameInput gameInput;
 
-  private bool isWalking;
+    private bool isWalking;
   private void Update()
   {
-    Vector2 inputVector = new Vector2(0, 0);
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
-    if (Input.GetKey(KeyCode.W))
-    {
-      inputVector.y = +1;
-    }
-    if (Input.GetKey(KeyCode.S))
-    {
-      inputVector.y = -1;
-    }
-    if (Input.GetKey(KeyCode.A))
-    {
-      inputVector.x = -1;
-    }
-    if (Input.GetKey(KeyCode.D))
-    {
-      inputVector.x = +1;
-    }
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += moveDir * moveSpeed * Time.deltaTime;
 
-    inputVector = inputVector.normalized;
+        isWalking = moveDir != Vector3.zero;
 
-    Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-    transform.position += moveDir * moveSpeed * Time.deltaTime;
-
-    isWalking = moveDir != Vector3.zero;
-
-    float rotateSpeed = 10f;
-    transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 
   }
 
